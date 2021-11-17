a667d0d3e45f3f768f5f6d3bcca5069dc2aaae5a
Karoline Godoy <karolinemellgodoy@gmail.com>
2021-11-17 09:30:55 -0300

    atualizacao exercicios

---
+++ Modulo_1/bloco_4/dia3/exercicios/corse.js
@@ -0,0 +1,18 @@
+// Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer,
+// seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
+// Por exemplo:
+
+// n = 5
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+let n = 8
+
+for(let i = 0 ; i < n ; i++ ){
+    console.log('*****')
+}
+    
---
+++ Modulo_1/bloco_4/dia4/conteudo/dia4.js
@@ -0,0 +1,11 @@
+let nome = 'João';
+
+console.log('Bom dia,' + nome);
+
+function bomDiaTryber(nome){
+    console.log('Bom dia,' + nome);
+}
+
+bomDiaTryber('João');
+bomDiaTryber('Julia');
+bomDiaTryber('Marcelo');
